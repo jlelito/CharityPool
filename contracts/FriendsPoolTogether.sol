2c7761e23ab4964b1944215c74a87624d02e52d2
jlelito <joshualelito@gmail.com>
2021-03-14 17:54:15 -0400

    Adding deposit and withdraw functions

--- contracts/FriendsPoolTogether.sol
+++ contracts/FriendsPoolTogether.sol
@@ -24,33 +24,41 @@ contract FriendsPoolTogether is CompoundWallet {
         uint poolID;
         string name;
         address payable admin;
+        uint amountDeposited;
     }
     
     //Creates a pool
     function createPool(string memory  _name) public {
-       pools[nextId] = Pool(nextId, _name,  msg.sender);
+       pools[nextId] = Pool(nextId, _name,  msg.sender, 0);
+       poolsWhitelist[msg.sender][nextId] = true;
        nextId++;
     }
     
     //Whitelist member for a pool
-    function whitelist(uint _id, address memberTarget) public onlyAdmin(_id) {
+    function whitelist(uint _id, address memberTarget) public onlyPoolAdmin(_id) {
         poolsWhitelist[memberTarget][_id] = true;
     }
     
     //Unwhitelist member for a pool
-    function unwhitelist(uint _id, address memberTarget) public onlyAdmin(_id) {
+    function unwhitelist(uint _id, address memberTarget) public onlyPoolAdmin(_id) {
         poolsWhitelist[memberTarget][_id] = false;
     }
     
     //Deposit into a pool
-    function deposit(uint _id) public payable onlyMember(_id) {
+    function deposit(uint _id, address payable compoundAddress) public payable onlyMember(_id) {
         deposits[msg.sender][_id] += msg.value;
+        pools[_id].amountDeposited += msg.value;
+        supplyEthToCompound(compoundAddress);
     }
     
     //Withdraw from a pool
-    function withdraw(uint _id, uint amount) public onlyMember(_id) {
-        deposits[msg.sender][_id] -= amount;
+    function withdraw(uint _id, uint amount, address compoundAddress) public onlyMember(_id) {
+        require(deposits[msg.sender][_id] >= amount, 'not enough deposited!');
+        redeemcETHTokens(amount, false, compoundAddress);
         address(msg.sender).transfer(amount);
+        deposits[msg.sender][_id] -= amount;
+        pools[0].amountDeposited -= amount;
+        
     }
     
     //Internal function: Releases the prize after the timeperiod
@@ -63,9 +71,10 @@ contract FriendsPoolTogether is CompoundWallet {
     function getRandomNumber() internal {
         //Get random number
     }
+    
 
     //Can only be admin for pool modifier
-    modifier onlyAdmin(uint _id) {
+    modifier onlyPoolAdmin(uint _id) {
         require(msg.sender == pools[_id].admin, 'Must be te admin for this pool!');
         _;
     }
