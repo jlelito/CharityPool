8ec6bf98134f8c398763516b13ec69fd8282936e
jlelito <joshualelito@gmail.com>
2021-03-15 22:03:08 -0400

    Adding ropsten and rinkeby networks

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,51 @@
+const path = require("path");
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
-  
-  contracts_build_directory: './src/abis/',
 
+  contracts_build_directory: path.join(__dirname, "src/abis"),
   networks: {
-    
-    development: {
-     host: "127.0.0.1",     
-     port: 7545,            
-     network_id: "*",       
+    develop: {
+      port: 7545
     },
-  },
 
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+
+    ropsten: {
+      provider: function(){
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`
+          )
+      },
+      gasPrice: 100000000000,
+      network_id: 3
+    },
+
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_API_KEY}`
+          )
+      },
+      gasPrice: 100000000000,
+      network_id: 4
+    },
+
+    
+  },
+  
   compilers: {
     solc: {
        version: "0.5.12",    
     }
   }
+  
+  
 }
