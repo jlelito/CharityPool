dc303164cb9812d7ab8e24c631f6a5e5d132a9a2
jlelito <joshualelito@gmail.com>
2021-03-16 21:50:03 -0400

    Adding withdraw function and adding ETH logo

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ethlogo from '../src_images/ETH.png';
 
 class Pool extends Component {
 
@@ -11,7 +12,9 @@ render() {
                     <h5 className='card-title'>{this.props.pool.name} </h5>
                     <div className='float-left'>
                         <p className='card-text'>Pool Admin: {this.props.pool.admin}  </p>
-                        <p className='card-text'>Total Amount Deposited: {this.props.pool.amountDeposited}</p>
+                        <p className='card-text'>Total Amount Deposited: {this.props.pool.amountDeposited} Wei
+                            <img className='my-2' src={ethlogo} width='25' height='25' alt='ethlogo'/>
+                        </p>
                         <p className='card-text'>Prize Interest Amount: </p>
                         <p className='card-text'>Next Prize Release Date: </p>
                         <div className='row justify-content-center'>
@@ -19,9 +22,7 @@ render() {
                         <form onSubmit={(e) => {
                             let depositAmount
                             e.preventDefault()
-                            console.log('Deposit Input:', this.depositInput.value)
                             depositAmount = this.depositInput.value.toString()
-                            console.log('Deposit amount:', depositAmount)
                             this.depositInput.value = null
                             this.props.poolDeposit(this.props.pool.poolID, depositAmount)
                         }}>
@@ -44,9 +45,8 @@ render() {
                             let withdrawAmount
                             e.preventDefault()
                             withdrawAmount = this.withdrawInput.value.toString()
-                            console.log('Withdraw amount:', withdrawAmount)
                             this.withdrawInput.value = null
-                            //this.props.poolWithdraw(this.props.pool.poolID, withdrawAmount)
+                            this.props.poolWithdraw(this.props.pool.poolID, withdrawAmount)
                         }}>
                             <input 
                                 type='number' 
