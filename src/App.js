e21cb787150b3c42fce02b8a490940e9b9060484
jlelito <joshualelito@gmail.com>
2021-03-22 22:02:01 -0400

    Adding whitelist and unwhitelist functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import FriendsPoolTogether from './abis/FriendsPoolTogether.json';
+import CharityPool from './abis/CharityPool.json';
 import Web3 from 'web3';
 import Navbar from './components/Navbar.js';
 import Notification from './components/Notification.js';
@@ -8,6 +8,8 @@ import Loading from './components/Loading.js';
 import ConnectionBanner from '@rimble/connection-banner';
 import cETH from './abis/cETHRopstenABI.json';
 import Pool from './components/Pool.js';
+import CharityVote from './components/CharityVote.js';
+import WhitelistForm from './components/WhitelistForm';
 
 class App extends Component {
 
@@ -58,15 +60,14 @@ class App extends Component {
 async loadContractData() {
   
   let contractAdmin, PoolTogetherData
-  PoolTogetherData = FriendsPoolTogether.networks[3]
+  PoolTogetherData = CharityPool.networks[3]
   if(PoolTogetherData) {
-    const abi = FriendsPoolTogether.abi
+    const abi = CharityPool.abi
     const address = PoolTogetherData.address
     //Load contract and set state
     const poolContract = new this.state.web3.eth.Contract(abi, address)
-    await this.setState({ poolContract, poolContractAddress: address })
-    contractAdmin = await this.state.poolContract.methods.admin().call()
-    this.setState({ admin: contractAdmin })
+    contractAdmin = await poolContract.methods.admin().call()
+    this.setState({ admin: contractAdmin, poolContract, poolContractAddress: address })
   }
   //Compound Ropsten address located here: https://compound.finance/docs#networks
   const compoundCETHContractAddress = '0x859e9d8a4edadfedb5a2ff311243af80f85a91b8'
@@ -74,28 +75,24 @@ async loadContractData() {
   await this.setState({cETHContract, cETHAddress: compoundCETHContractAddress})
   let cETHBalance = await this.state.cETHContract.methods.balanceOf(this.state.poolContractAddress).call()
   this.setState({contractCETHBalance: cETHBalance})
-  console.log('cETH Contract:', this.state.cETHContract)
   let contractETHDeposited = await this.state.cETHContract.methods.balanceOfUnderlying(this.state.poolContractAddress).call()
   contractETHDeposited = this.state.web3.utils.fromWei(contractETHDeposited, 'Ether')
   this.setState({ contractETHDeposited })
 
-
 }
 
 async loadPoolData() {
-
   let currentDeposit = await this.state.poolContract.methods.deposits(this.state.account).call()
-  
   this.setState({depositedAmount: currentDeposit})
 }
 
 
 
 //Supply ETH to Pool
-poolDeposit = (id, amount) => {
+poolDeposit = (amount) => {
   amount = this.state.web3.utils.toHex(this.state.web3.utils.toWei(amount, 'ether'))
   try {
-    this.state.poolContract.methods.deposit(id, this.state.cETHAddress).send({ from: this.state.account, value: amount}).on('transactionHash', async (hash) => {
+    this.state.poolContract.methods.deposit(this.state.cETHAddress).send({ from: this.state.account, value: amount}).on('transactionHash', async (hash) => {
        this.setState({hash: hash, action: 'Deposited ETH to Pool', trxStatus: 'Pending', confirmNum: 0})
        this.showNotification()
 
@@ -126,10 +123,10 @@ poolDeposit = (id, amount) => {
 }
 
 //Withdraw ETH from Pool
-poolWithdraw = (id, amount) => {
+poolWithdraw = (amount) => {
   amount = this.state.web3.utils.toHex(this.state.web3.utils.toWei(amount, 'ether'))
   try {
-    this.state.poolContract.methods.withdraw(id, amount, this.state.cETHAddress).send({ from: this.state.account }).on('transactionHash', async (hash) => {
+    this.state.poolContract.methods.withdraw(amount, this.state.cETHAddress).send({ from: this.state.account }).on('transactionHash', async (hash) => {
        this.setState({hash: hash, action: 'Redeemed ETH from Pool', trxStatus: 'Pending', confirmNum: 0})
        this.showNotification()
 
@@ -157,6 +154,75 @@ poolWithdraw = (id, amount) => {
     catch(e) {
       window.alert(e)
     }
+}
+
+poolWhitelist = (targetAddress) => {
+  try {
+    this.state.poolContract.methods.whitelist(targetAddress).send({ from: this.state.account }).on('transactionHash', async (hash) => {
+       this.setState({hash: hash, action: 'Whitelisted Member to Pool', trxStatus: 'Pending', confirmNum: 0})
+       this.showNotification()
+
+      }).on('receipt', async (receipt) => {
+          await this.loadAccountData()
+          await this.loadContractData()
+          await this.loadPoolData()
+
+          if(receipt.status === true){
+            this.setState({trxStatus: 'Success'})
+          }
+          else if(receipt.status === false){
+            this.setState({trxStatus: 'Failed'})
+          }
+      }).on('error', (error) => {
+          window.alert('Error! Could not whitelist member!')
+      }).on('confirmation', (confirmNum) => {
+          if(confirmNum > 10) {
+            this.setState({confirmNum : '10+'})
+          } else {
+          this.setState({confirmNum})
+          }
+      })
+    }
+    catch(e) {
+      window.alert(e)
+    }
+}
+
+poolUnwhitelist = (targetAddress) => {
+  try {
+    this.state.poolContract.methods.unwhitelist(targetAddress).send({ from: this.state.account }).on('transactionHash', async (hash) => {
+       this.setState({hash: hash, action: 'Unwhitelisted Member from Pool', trxStatus: 'Pending', confirmNum: 0})
+       this.showNotification()
+
+      }).on('receipt', async (receipt) => {
+          await this.loadAccountData()
+          await this.loadContractData()
+          await this.loadPoolData()
+
+          if(receipt.status === true){
+            this.setState({trxStatus: 'Success'})
+          }
+          else if(receipt.status === false){
+            this.setState({trxStatus: 'Failed'})
+          }
+      }).on('error', (error) => {
+          window.alert('Error! Could not unwhitelist member!')
+      }).on('confirmation', (confirmNum) => {
+          if(confirmNum > 10) {
+            this.setState({confirmNum : '10+'})
+          } else {
+          this.setState({confirmNum})
+          }
+      })
+    }
+    catch(e) {
+      window.alert(e)
+    }
+}
+
+
+vote = (id, voteAmount) => {
+  console.log('voting')
 }
 
 showNotification = () => {
@@ -239,25 +305,39 @@ constructor(props) {
           />
           &nbsp;
           <div className='mt-3'></div>
-          <h1 className='mt-1 mb-5'>Charity Pool</h1>
-          <p>Pool Money together for Charity! Vote on the charity of the week to receive the interest!</p>
+          <h1 className='mt-1 mb-3'>Charity Pool</h1>
+          <p>Pool money together for Charity! Vote on the charity of the week to receive the interest!</p>
+
+          {this.state.admin === this.state.account ? 
+          
+          <WhitelistForm 
+            poolWhitelist={this.poolWhitelist}
+            poolUnwhitelist={this.poolUnwhitelist}
+          
+          />
+          
+          : null
+
+          }
+
+
           <h3 className='mt-5'>ETH Deposited to Contract: {this.state.contractETHDeposited} ETH</h3>
           <h3 className='mt-2'>Contract cETH Balance: {this.state.contractCETHBalance} cETH</h3>
-          {this.state.poolsList.length === 0 ? <h1 className='my-5'>No Pools Found!</h1> : 
-            <>
-              <div className='row'>
-                <Pool
-                  key={pool.poolID}
-                  web3={this.state.web3}
-                  depositedAmount={this.state.depositedAmount}
-                  poolDeposit={this.poolDeposit}
-                  poolWithdraw={this.poolWithdraw}
-                  currentEthBalance = {this.state.currentEthBalance}
-                /> 
-              </div>
-            </>
-            }
-          
+            <div className='row justify-content-center'>
+              <Pool
+                web3={this.state.web3}
+                admin={this.state.admin}
+                depositedAmount={this.state.depositedAmount}
+                poolDeposit={this.poolDeposit}
+                poolWithdraw={this.poolWithdraw}
+                currentEthBalance = {this.state.currentEthBalance}
+              /> 
+            </div>
+
+            <div className='row mt-5 justify-content-center'>
+              <CharityVote />
+            </div>
+
           <div className='row justify-content-center mt-4'>
             <p>Charity Pool Contract on Etherscan: </p>
             <a className='ml-3' href={`https://ropsten.etherscan.io/address/${this.state.poolContractAddress}`} target='_blank'>Etherscan</a>
