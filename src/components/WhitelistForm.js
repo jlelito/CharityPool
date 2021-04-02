694a62a74d569be5cee3648c9fafa7067413e667
jlelito <joshualelito@gmail.com>
2021-04-01 22:51:06 -0400

    Removing onSubmit attribute

--- src/components/WhitelistForm.js
+++ src/components/WhitelistForm.js
@@ -5,29 +5,23 @@ class WhitelistForm extends Component {
     render() {
         return (
             <>
-            <form onSubmit={(e) => {
-              let targetAddress
-              e.preventDefault()
-              targetAddress = this.targetAddress.value.toString()
-              this.targetAddress.value = null
-              this.props.poolWhitelist(targetAddress)
-            }}>
-              <div className='row justify-content-center'>
-              <input 
-                  type='text' 
-                  className='form-control mx-2 col-4' 
-                  placeholder='0x000...' 
-                  ref={(targetAddress) => { this.targetAddress = targetAddress }}
-                  disabled={this.props.isConnected}
-                  required 
-              />
-              </div>
-              <div className='row justify-content-center'>
-                  <button className='btn btn-primary mt-1' type='submit'>Whitelist</button>
-                  <button className='btn btn-primary mt-1 mx-2' onClick={() => this.props.poolUnwhitelist(this.targetAddress.value.toString())}>Unwhitelist</button>
-              </div>
-            </form>
-          </>
+              <form>
+                <div className='row justify-content-center'>
+                <input 
+                    type='text' 
+                    className='form-control mx-2 col-4' 
+                    placeholder='0x000...' 
+                    ref={(targetAddress) => { this.targetAddress = targetAddress }}
+                    disabled={this.props.isConnected}
+                    required 
+                />
+                </div>
+                <div className='row justify-content-center'>
+                    <button className='btn btn-primary mt-1' type='button' onClick={() => this.props.poolWhitelist(this.targetAddress.value.toString())}>Whitelist</button>
+                    <button className='btn btn-primary mt-1 mx-2' type='button' onClick={() => this.props.poolUnwhitelist(this.targetAddress.value.toString())}>Unwhitelist</button>
+                </div>
+              </form>
+            </>
         );
     }
   }
