916a81cfdc2f7dccecd5d40dcb8a33b05d55f7cd
jlelito <joshualelito@gmail.com>
2021-04-05 21:14:53 -0400

    Clearing inputs when whitelisted

--- src/components/WhitelistForm.js
+++ src/components/WhitelistForm.js
@@ -17,8 +17,15 @@ class WhitelistForm extends Component {
                 />
                 </div>
                 <div className='row justify-content-center'>
-                    <button className='btn btn-primary mt-1' type='button' onClick={() => this.props.poolWhitelist(this.targetAddress.value.toString())}>Whitelist</button>
-                    <button className='btn btn-primary mt-1 mx-2' type='button' onClick={() => this.props.poolUnwhitelist(this.targetAddress.value.toString())}>Unwhitelist</button>
+                    <button className='btn btn-primary mt-1' type='button' onClick={() => {
+                        this.props.poolWhitelist(this.targetAddress.value.toString())
+                        this.targetAddress.value = null
+                    }}>Whitelist</button>
+                    <button className='btn btn-primary mt-1 mx-2' type='button' onClick={() => {
+                        this.props.poolUnwhitelist(this.targetAddress.value.toString())
+                        this.targetAddress.value = null
+                    }}>Unwhitelist</button>
+                      
                 </div>
               </form>
             </>
