b56b28f6f3934570178507ca2f6fed6df2bc869d
jlelito <joshualelito@gmail.com>
2021-04-11 10:45:35 -0400

    Conversion rate UI changes

--- src/App.js
+++ src/App.js
@@ -566,14 +566,18 @@ constructor(state) {
             </>
             :  null}
             <div className='row justify-content-center'>
-              <div className='col-auto' className='text-muted small'>
+              <div className='col-auto' className='text-muted small mr-2'>
                 Conversion Rate: 
               </div>
               <div className='col-auto float-left'>
-                <li className='text-muted small'>.001 ETH = 1 Vote</li>
-                <li className='text-muted small'>.01 ETH = 10 Votes</li>
-                <li className='text-muted small'>.1 ETH = 100 Votes</li>
-                <li className='text-muted small'>1 ETH = 1000 Votes</li>
+                <div className='row justify-content-center'>
+                  <p className='text-muted small'>.001 ETH = 1 Vote</p>
+                  <p className='text-muted small ml-3'>.01 ETH = 10 Votes</p>
+                </div>
+                <div className='row justify-content-center'>
+                  <p className='text-muted small'>.1 ETH = 100 Votes</p>
+                  <p className='text-muted small ml-3'>1 ETH = 1000 Votes</p>
+                </div>
               </div>
             </div>
             
